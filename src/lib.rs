d001c6fe1fc27c52322b271c71266e1c63f34a34
Relrige <91017454+Relrige@users.noreply.github.com>
2025-11-05 22:40:23 +0200

    Add errors, and ast import

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 use pest::{Parser};
 use thiserror::Error;
 
+pub mod ast;
+use ast::{Controller, ControllerMethod};
+
 #[derive(pest_derive::Parser)]
 #[grammar = "./grammar.pest"]
 pub struct Grammar;
 
 #[derive(Error, Debug)]
 pub enum SpringControllerError {
-    
+    #[error("parsing error: {0}")]
+    PestError(#[from] pest::error::Error<Rule>),
+    #[error("no controllers found error")]
+    NoControllers,
 }
 
 
