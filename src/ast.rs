e13d76ced8dc004970328d92ad4d1631078cfd3f
Relrige <91017454+Relrige@users.noreply.github.com>
2025-11-05 19:16:20 +0200

    Add AST representing parse result ,  fix grammar, add base error enum

---
+++ src/ast.rs
@@ -0,0 +1,15 @@
+
+// Representaion of Spring Controller in Java
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Controller {
+    pub name: String,
+    pub class_mapping: Option<String>,
+    pub methods: Vec<ControllerMethod>,
+}
+// Representation of Controller Method in Spring Java
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ControllerMethod {
+    pub annotation: Option<String>,
+    pub annotation_args: Option<String>,
+    pub header: String,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 use pest::{Parser};
+use thiserror::Error;
 
 #[derive(pest_derive::Parser)]
 #[grammar = "./grammar.pest"]
 pub struct Grammar;
+
+#[derive(Error, Debug)]
+pub enum SpringControllerError {
+    
+}
 
 
 
