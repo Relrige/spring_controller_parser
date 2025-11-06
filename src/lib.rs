351aa2da8b5d0b321acc152f594b6577c31854a1
Relrige <91017454+Relrige@users.noreply.github.com>
2025-11-06 22:58:37 +0200

    Start working on parse

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use pest::{Parser};
+use pest::{Parser, iterators::Pair};
 use thiserror::Error;
 
 pub mod ast;
@@ -9,12 +9,26 @@ use ast::{Controller, ControllerMethod};
 pub struct Grammar;
 
 #[derive(Error, Debug)]
-pub enum SpringControllerError {
+pub enum SpringControllerParserError {
     #[error("parsing error: {0}")]
     PestError(#[from] pest::error::Error<Rule>),
     #[error("no controllers found error")]
     NoControllers,
 }
 
+pub fn parse_controllers(src: &str) -> Result<Vec<Controller>, SpringControllerParserError> {
+    let parse_result = Grammar::parse(Rule::controller_file, src)?;
+    let controllers: Vec<_> = parse_result
+        .filter_map(|pair| extract_controller(pair))
+        .collect();
 
+    if controllers.is_empty() {
+        Err(SpringControllerParserError::NoControllers)
+    } else {
+        Ok(controllers)
+    }
+}
 
+fn extract_controller(pair: Pair<Rule>) -> Option<Controller> {
+   None
+}
