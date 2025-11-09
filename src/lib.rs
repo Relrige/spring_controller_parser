1a150748f15b95402c4b21f10e07bb79dd62fbe1
Relrige <91017454+Relrige@users.noreply.github.com>
2025-11-09 16:42:07 +0200

    Continue working on parser

--- src/lib.rs
+++ src/lib.rs
@@ -30,5 +30,43 @@ pub fn parse_controllers(src: &str) -> Result<Vec<Controller>, SpringControllerP
 }
 
 fn extract_controller(pair: Pair<Rule>) -> Option<Controller> {
-   None
+    let parsed = pair.as_str();
+    if !(parsed.contains("@Controller") || parsed.contains("@RestController")) {
+        return None;
+    }
+
+    let name = extract_name(parsed)?;
+    let class_mapping = extract_mapping(parsed);
+    let methods = extract_methods(parsed);
+
+    Some(Controller { name, class_mapping, methods })
+}
+
+fn extract_name(text: &str) -> Option<String> {
+    let words: Vec<&str> = text.split_whitespace().collect();
+
+    for i in 0..words.len() {
+        if words[i] == "class" {
+            if let Some(name) = words.get(i + 1) {
+                let clean_name = name.trim_end_matches('{').to_string();
+                return Some(clean_name);
+            }
+        }
+    }
+    None
+}
+
+fn extract_mapping(text: &str) -> Option<String> {
+    if let Some(start) = text.find("@RequestMapping") {
+        let after_part = &text[start..];
+        let part_before = after_part.split(')').next().unwrap_or("");
+        return Some(part_before.to_string());
+    }
+    None
+}
+
+fn extract_methods(s: &str) -> Vec<ControllerMethod> {
+    let mut methods = Vec::new();
+
+    methods
 }
